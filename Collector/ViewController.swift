ab513c5230f9dd1cc53dab3de2cb7236fe604230
Rico Alderete <su8lim3@gmail.com>
2019-05-04 11:51:03 -0600

    Initial Commit

---
+++ Collector/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Collector
+//
+//  Created by Rico Alderete on 5/4/19.
+//  Copyright © 2019 Rico Alderete. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
